d80c8651800e63723fa2f6a34e5ad2d6a595c004
runphp <runphp@qq.com>
2017-12-08 17:16:55 +0800

    json decode 问题修复

--- src/Swallow/Service/Application.php
+++ src/Swallow/Service/Application.php
@@ -652,7 +652,8 @@ class Application extends \Phalcon\Mvc\Application
                 ]);
                 if (200 == $data->getStatusCode()) {
                     //$res = json_decode((string)$data->getBody(), true);
-                    $res = \Swallow\Toolkit\Util\Json::decode2((string)$data->getBody());
+                    //$res = \Swallow\Toolkit\Util\Json::decode2((string)$data->getBody());
+                    $this->isPhinx ? \Swallow\Toolkit\Util\Json::decode2((string)$data->getBody()) : json_decode((string)$data->getBody(), true);
                 } else {
                     $res = [
                         'status' => $data->getStatusCode(),
