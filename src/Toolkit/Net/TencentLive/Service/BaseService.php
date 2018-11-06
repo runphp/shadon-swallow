39bfba82483af49e83d29a4d6d0e9e5d64ace161
wechan <a421083@qq.com>
2018-11-06 14:09:19 +0800

    直播更换推流和播放地址

--- src/Toolkit/Net/TencentLive/Service/BaseService.php
+++ src/Toolkit/Net/TencentLive/Service/BaseService.php
@@ -17,16 +17,16 @@ class BaseService extends Service
 {
     protected $ttl = 86400;
 
-    protected $pushUrl = 'rtmp://%s.livepush.myqcloud.com/live/%s?%s';
+    protected $pushUrl = 'rtmp://push.eelly.com/live/%s?%s';
 
     protected $bizId = '3344';
 
     protected $pushKey = '4bda0c7a4fca42abdc7767f5f4d2d4f2';
 
     protected $playUrl = [
-        'RTMP' => 'rtmp://%s.liveplay.myqcloud.com/live/%s',
-        'FLV' => 'http://%s.liveplay.myqcloud.com/live/%s.flv',
-        'HLS' => 'http://%s.liveplay.myqcloud.com/live/%s.m3u8',
+        'RTMP' => 'rtmp://play.eelly.com/live/%s',
+        'FLV' => 'http://play.eelly.com/live/%s.flv',
+        'HLS' => 'http://play.eelly.com/live/%s.m3u8',
     ];
 
     protected function init()
@@ -44,9 +44,14 @@ class BaseService extends Service
     public function getPushUrl(string $liveId)
     {
         $txTime = strtoupper(base_convert(time() + $this->ttl, 10, 16));
+        // livecode = bizid+"_"+env+"_"+stream_id 如 8888_test_123456
+        // 直播码
+        $livecode = sprintf('%s_%s_%s',
+                $this->bizId,
+                getenv('APPLICATION_ENV') ?: 'prod',
+                $liveId
+            );
         // txSecret = MD5( KEY + livecode + txTime )
-        // livecode = bizid+"_"+stream_id 如 8888_test123456
-        $livecode = $this->bizId . "_" . $liveId; // 直播码
         $txSecret = md5($this->pushKey . $livecode . $txTime);
         $extStr = http_build_query([
             "bizid" => $this->bizId,
@@ -54,7 +59,6 @@ class BaseService extends Service
             "txTime" => $txTime
         ]);
         $pushUrl = sprintf($this->pushUrl,
-                $this->bizId,
                 $livecode,
                 $extStr
             );
@@ -71,12 +75,15 @@ class BaseService extends Service
     public function getPlayUrl(string $liveId)
     {
         $playUrl = [];
-        $liveCode = $this->bizId . "_" . $liveId;
+        $liveCode = sprintf('%s_%s_%s',
+                $this->bizId,
+                getenv('APPLICATION_ENV') ?: 'prod',
+                $liveId
+            );
         foreach ($this->playUrl as $type => $url){
             $playUrl[$type] = sprintf($url,
-                    $this->bizId,
-                    $liveCode
-                );
+                $liveCode
+            );
         }
 
         return $playUrl;
